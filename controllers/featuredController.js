fd1f21e854827281945e96cca6cbdf228b1a9927
anja007 <popovicanja972@gmail.com>
2025-08-19 11:56:25 +0200

    final changes

--- controllers/featuredController.js
+++ controllers/featuredController.js
@@ -20,7 +20,6 @@ const getFeatured = async (req, res) => {
         tmdbId: item.tmdbId,
         mediaType: item.mediaType,
         listName: item.listName,
-        sortOrder: item.sortOrder,
         title: data.title || data.name,
         posterPath: data.poster_path,
         voteAverage: data.vote_average,
@@ -35,63 +34,49 @@ const getFeatured = async (req, res) => {
   }
 };
 
-// POST featured (dodavanje)
-/*
-const addFeatured = async (req, res) => {
-  const { tmdbId, mediaType, listName, sortOrder } = req.body;
-  if (!tmdbId || !mediaType)
-    return res.status(400).json({ message: "tmdbId i mediaType are required" });
-
-  try {
-    const existing = await checkRecordExists("featured", "tmdbId", tmdbId);
-    if (existing) return res.status(400).json({ message: "Featured already exists" });
-
-    await insertRecord("featured", { tmdbId, mediaType, listName, sortOrder: sortOrder || 1 });
-    res.status(201).json({ message: "Featured added" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error", error: error.message });
-  }
-};*/
-
-
 const addFeatured = async (req, res) => {
   try {
-    const { title, mediaType, listName, sortOrder } = req.body;
+    const { title, mediaType, listName } = req.body;
 
     if (!title || !mediaType) {
-      return res.status(400).json({ message: "Title i mediaType su obavezni" });
+      return res.status(400).json({ message: "Title and mediaType are required!" });
     }
+
     const searchUrl = `https://api.themoviedb.org/3/search/${mediaType}?api_key=${process.env.API_TMDB_KEY}&query=${encodeURIComponent(title)}`;
     const tmdbRes = await axios.get(searchUrl);
 
     if (!tmdbRes.data.results || tmdbRes.data.results.length === 0) {
-      return res.status(404).json({ message: "Nije pronađen film/serija na TMDB" });
+      return res.status(404).json({ message: "Not found content in TMDB" });
     }
 
     const firstResult = tmdbRes.data.results[0];
     const tmdbId = firstResult.id;
 
-    await insertRecord("featured", {
-      tmdbId,
-      mediaType,
-      listName,
-      sortOrder,
-    });
+    const existingRecords = await getAllRecords("featured");
+    const alreadyInList = existingRecords.find(
+      item => item.tmdbId === tmdbId && item.listName === listName
+    );
 
-    res.status(201).json({ message: "Featured item dodat", tmdbId });
+    if (alreadyInList) {
+      return res.status(400).json({ message: "This item is already in the selected list!" });
+    }
+
+    await insertRecord("featured", { tmdbId, title, mediaType, listName });
+
+    res.status(201).json({ message: "Featured item added", tmdbId });
+    
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Greška pri dodavanju featured item-a" });
+    console.error("Error:", err);
+    res.status(500).json({ message: "Error while adding featured item", error: err.message || err });
   }
 };
 
 const updateFeatured = async (req, res) => {
     const { id } = req.params;
-    const { sortOrder, listName } = req.body;
+    const { listName } = req.body;
 
     try {
-        await updateRecord("featured", "featuredId", id, { sortOrder, listName });
+        await updateRecord("featured", "featuredId", id, {listName });
         res.json({ message: "Featured updated" });
 
     } catch (error) {
@@ -103,8 +88,11 @@ const updateFeatured = async (req, res) => {
 const deleteFeatured = async (req, res) => {
   const { id } = req.params;
   try {
-    await deleteRecord("featured", "featuredId", id);
-    res.json({ message: "Featured deleted" });
+    const result = await deleteRecord("featured", "featuredId", id);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "No record found with this id" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error", error: error.message });
